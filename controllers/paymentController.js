ea0901fd1561161fe67e4840b2ffabd3beb8ce94
Victor <Victor@gmail.com>
2024-07-24 12:16:31 +0100

    url

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -22,7 +22,7 @@ exports.initiatePayment = async (req, res) => {
             tx_ref: orderId,
             amount,
             currency,
-            redirect_url: "https://startech-ecom-api-elc7.onrender.com/thankyou",
+            redirect_url: "https://startech-ecom-test.vercel.app/thankyou",
             customer: {
                 email: user.email,
                 name: `${user.firstName} ${user.lastName}`,
